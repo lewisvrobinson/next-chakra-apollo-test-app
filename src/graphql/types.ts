a2645e221b703d98e5175851b67478a1b3e3e70a
Lewis Robinson <lewisvrobinson@users.noreply.github.com>
2024-07-07 22:46:10 +1000

    Add response type

---
+++ src/graphql/types.ts
@@ -0,0 +1,28 @@
+export type CharacterOrigin = {
+  name: string;
+};
+
+export type Character = {
+  id: string;
+  name: string;
+  image: string;
+  status: string;
+  species: string;
+  origin: CharacterOrigin;
+};
+
+// Used to define the pagination information
+export type PageInfo = {
+  count: number;
+  pages: number;
+  next: number | null;
+  prev: number | null;
+};
+
+// The type for the expected characters query response
+export type CharactersResponse = {
+  characters: {
+    info: PageInfo;
+    results: Character[];
+  };
+};
