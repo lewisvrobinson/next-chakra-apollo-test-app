c948665e56682bdbf2b9204b4ed10e0535947c69
Lewis Robinson <lewisvrobinson@users.noreply.github.com>
2024-07-08 01:24:17 +1000

    Rename route

---
+++ src/app/characters/page.tsx
@@ -0,0 +1,14 @@
+import { VStack, Heading } from "@chakra-ui/react";
+import { CharacterList } from "./list";
+
+
+export default function ListPage() {
+  return (
+    <VStack as="main" gap={8} p={8} maxWidth="4xl" marginX="auto">
+      <Heading as="h1" size="2xl">
+        List Page
+      </Heading>
+      <CharacterList />
+    </VStack>
+  );
+}
