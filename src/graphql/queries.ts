1c53a22e031247fe071301f09433820ed4778396
Lewis Robinson <lewisvrobinson@users.noreply.github.com>
2024-07-07 22:45:18 +1000

    Define character queries

---
+++ src/graphql/queries.ts
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const GET_PAGINATED_CHARACTERS = gql`
+  query GetPaginatedCharacters($page: Int) {
+    characters(page: $page) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+export const GET_CHARACTER_BY_ID = gql`
+  query GetCharacterById($id: ID!) {
+    character(id: $id) {
+      id
+      name
+      image
+      location {
+        name
+      }
+      species
+      status
+      type
+    }
+  }
+`
